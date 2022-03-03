38475f26e6db9593b72b2cb009f908fbee5b4dfd
Muhammed H. Karamuk <a_developer_2002@outlook.com>
2022-03-03 07:10:09 +0300

    update

--- frontend/src/components/profileTemplate.jsx
+++ frontend/src/components/profileTemplate.jsx
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGear, faXmark } from '@fortawesome/free-solid-svg-icons';
 import FTButton from './ftbutton';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -10,9 +9,44 @@ export default function ProfileTemplate(props)
 			<img className="w-full" src="https://avatars.githubusercontent.com/u/25377153?v=4" alt="Sunset in the mountains"></img>
 				<div className="px-6 py-4">
 					<div className="font-bold text-xl mb-2">Login42 Proifl Bilgileri</div>
-					<p className="text-gray-700 text-base">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-					</p>
+					<table className = "border-1 w-full">
+						<tr>
+							<td className = "border-1 w-2/5">
+								<span className = "font-bold">
+									Tam İsim :
+								</span>
+							</td>
+							<td className = "border-1 w-3/5">
+								<span>
+									k.adi
+								</span>
+							</td>
+						</tr>
+						<tr>
+							<td className = "border-1 w-2/5">
+								<span className = "font-bold">
+									E-mail :
+								</span>
+							</td>
+							<td className = "border-1 w-3/5">
+								<span>
+									student@42istanbul.com.tr
+								</span>
+							</td>
+						</tr>
+						<tr>
+							<td className = "border-1 w-2/5">
+								<span className = "font-bold">
+									Roller :
+								</span>
+							</td>
+							<td className = "border-1 w-3/5">
+								<span>
+									Otostopçu
+								</span>
+							</td>
+						</tr>
+					</table>
 				</div>
 				<div className="px-6 pt-4 pb-2 flex flex-col">
 					<FTButton className="text-white"
